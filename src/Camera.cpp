b28697e97a98c74503cb3604a1f372b9849f2fc3
Hang Yeung <yeungh@umich.edu>
2025-06-01 19:48:19 -0400

    Fix ray cast origin

--- src/Camera.cpp
+++ src/Camera.cpp
@@ -75,26 +75,26 @@ void hls::Camera::render(const Scene &scene) const {
 std::generator<hls::Ray> hls::Camera::rays() const {
     glm::vec3 forward = glm::normalize(target - position);
     glm::vec3 right   = glm::normalize(glm::cross(forward, up));
-    glm::vec3 up      = glm::cross(right, forward);
+    glm::vec3 true_up = glm::cross(right, forward);
 
     float pixel_width  = viewport_width / image_width;
     float pixel_height = viewport_height / image_height;
 
     glm::vec3 delta_u = right * pixel_width;
-    glm::vec3 delta_v = -up * pixel_height;
+    glm::vec3 delta_v = -true_up * pixel_height;
 
     glm::vec3 base_pixel =
-        position + forward * focal_length                // center
-        + right * (-viewport_width + pixel_width) * 0.5f // justify left
-        + up * (viewport_height + pixel_height) * 0.5f;  // justify top
+        position + forward * focal_length                    // center
+        - right * (viewport_width - pixel_width) * 0.5f      // justify left
+        + true_up * (viewport_height - pixel_height) * 0.5f; // justify top
 
     for (unsigned int y = 0; y < image_height; ++y) {
         for (unsigned int x = 0; x < image_width; ++x) {
-            glm::vec3 origin = base_pixel                         //
+            glm::vec3 pixel = base_pixel                         //
                                + static_cast<float>(x) * delta_u  //
                                + static_cast<float>(y) * delta_v; //
-            glm::vec3 direction = glm::normalize(origin - position);
-            co_yield Ray(origin, direction);
+            glm::vec3 direction = glm::normalize(pixel - position);
+            co_yield Ray(position, direction);
         }
     }
 }

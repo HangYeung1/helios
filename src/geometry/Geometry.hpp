f10923f49dfc3269b3a07691a14641e190e29f79
Hang Yeung <yeungh@umich.edu>
2025-06-04 00:27:19 -0400

    Add hittable and scatterable concepts

--- src/geometry/Geometry.hpp
+++ src/geometry/Geometry.hpp
@@ -2,10 +2,26 @@
 
 #include "Sphere.hpp"
 
+#include <concepts>
+#include <optional>
 #include <variant>
 
 namespace hls {
 
 using Geometry = std::variant<Sphere>;
+
+template <typename T>
+concept Hittable = requires(const T &obj, const Ray &ray) {
+    { obj.hit(ray) } -> std::same_as<std::optional<Ray>>;
+};
+
+template <typename Variant>
+inline constexpr bool hittable_variant = false;
+
+template <typename... Ts>
+inline constexpr bool hittable_variant<std::variant<Ts...>> =
+    (Hittable<Ts> && ...);
+
+static_assert(hittable_variant<Geometry>);
 
 } // namespace hls
--- src/material/Material.hpp
+++ src/material/Material.hpp
@@ -2,10 +2,26 @@
 
 #include "NaiveDiffuse.hpp"
 
+#include <concepts>
 #include <variant>
 
 namespace hls {
 
 using Material = std::variant<NaiveDiffuse>;
+
+template <typename T>
+concept Scatterable = requires(
+    const T &obj, const Ray &ray, glm::vec3 &radiance, glm::vec3 &throughput) {
+    { obj.scatter(ray, radiance, throughput) } -> std::same_as<Ray>;
+};
+
+template <typename Variant>
+inline constexpr bool scatterable_variant = false;
+
+template <typename... Ts>
+inline constexpr bool scatterable_variant<std::variant<Ts...>> =
+    (Scatterable<Ts> && ...);
+
+static_assert(scatterable_variant<Material>);
 
 } // namespace hls

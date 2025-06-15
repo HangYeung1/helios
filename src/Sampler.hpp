4c1098e6507b083275bbc3eb5c831b3afbb2de79
Hang Yeung <yeungh@umich.edu>
2025-06-15 02:50:47 -0400

    Add owen scrambling

--- src/Sampler.hpp
+++ src/Sampler.hpp
@@ -4,6 +4,7 @@
 
 #include <array>
 #include <cmath>
+#include <cstdint>
 #include <ranges>
 #include <stdexcept>
 
@@ -26,6 +27,7 @@ class Sampler {
                     bits ^= constants::SOBOL_DIRECTIONS[dimension][i];
                 }
             }
+            bits   = scramble(bits);
             sample = static_cast<float>(bits) / std::pow(2.0f, 32.0f);
             ++dimension;
         }
@@ -43,6 +45,32 @@ class Sampler {
     }
 
   private:
+    static constexpr inline uint32_t lk_hash(uint32_t x) {
+        x += seed;
+        x ^= x * 0x6c50b47cu;
+        x ^= x * 0xb82f1e52u;
+        x ^= x * 0xc7afe638u;
+        x ^= x * 0x8d22f6e6u;
+        return x;
+    }
+
+    static constexpr inline uint32_t reverse_bits(uint32_t x) {
+        x = ((x >> 1) & 0x55555555) | ((x << 1) & 0xAAAAAAAA);
+        x = ((x >> 2) & 0x33333333) | ((x << 2) & 0xCCCCCCCC);
+        x = ((x >> 4) & 0x0F0F0F0F) | ((x << 4) & 0xF0F0F0F0);
+        x = ((x >> 8) & 0x00FF00FF) | ((x << 8) & 0xFF00FF00);
+        x = (x >> 16) | (x << 16);
+        return x;
+    }
+
+    static constexpr inline uint32_t scramble(uint32_t x) {
+        x = reverse_bits(x);
+        x = lk_hash(x);
+        x = reverse_bits(x);
+        return x;
+    }
+
+    static constexpr uint32_t              seed      = 42;
     static thread_local inline std::size_t index     = 0;
     static thread_local inline std::size_t dimension = 0;
 };

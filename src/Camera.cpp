70bed96ca68fa6fd6cf537fa9964e947b0aadfe1
Hang Yeung <yeungh@umich.edu>
2025-06-24 14:52:50 -0400

    Refactor sub-sampling with std::transform_reduce

--- src/Camera.cpp
+++ src/Camera.cpp
@@ -8,6 +8,7 @@
 #include <glm/vec3.hpp>
 #include <numeric>
 #include <print>
+#include <ranges>
 #include <vector>
 
 void hls::Camera::render(const Scene &scene) const {
@@ -25,48 +26,51 @@ void hls::Camera::render(const Scene &scene) const {
                               - right * viewport_width * 0.5f
                               + true_up * viewport_height * 0.5f;
 
-    std::vector<std::size_t> indicies(image_width * image_height);
-    std::iota(indicies.begin(), indicies.end(), 0);
-
+    // Render
+    auto pixel_indicies = std::views::iota(0u, image_width * image_height);
     std::vector<glm::vec3> pixels(image_width * image_height);
-    std::transform(std::execution::par_unseq,
-                   indicies.begin(),
-                   indicies.end(),
-                   pixels.begin(),
-                   [&, this](std::size_t index) {
-                       Sampler sampler(index * pixel_samples);
+    std::transform(
+        std::execution::par_unseq,
+        pixel_indicies.begin(),
+        pixel_indicies.end(),
+        pixels.begin(),
+        [&, this](std::size_t pixel_index) {
+            std::size_t x = pixel_index % image_width;
+            std::size_t y = pixel_index / image_width;
 
-                       std::size_t x = index % image_width;
-                       std::size_t y = index / image_width;
+            glm::vec3 basis = matrix_origin + static_cast<float>(x) * delta_u
+                              + static_cast<float>(y) * delta_v;
 
-                       glm::vec3 accumulated(0);
-                       glm::vec3 basis = matrix_origin
-                                         + static_cast<float>(x) * delta_u
-                                         + static_cast<float>(y) * delta_v;
+            auto      sample_indicies = std::views::iota(0u, pixel_samples);
+            glm::vec3 accumulated     = std::transform_reduce(
+                std::execution::par_unseq,
+                sample_indicies.begin(),
+                sample_indicies.end(),
+                glm::vec3(0.0f),
+                std::plus<glm::vec3>(),
+                [&, this](std::size_t sample_index) {
+                    Sampler sampler(pixel_index * pixel_samples + sample_index);
+                    auto [r1, r2]  = sampler.sample<2>();
+                    glm::vec3 cast = basis + r1 * delta_u + r2 * delta_v;
+                    Ray       ray(position, glm::normalize(cast - position));
 
-                       for (std::size_t _ = 0; _ < pixel_samples; ++_) {
-                           auto [r1, r2]  = sampler.sample<2>();
-                           glm::vec3 cast = basis + r1 * delta_u + r2 * delta_v;
-                           Ray ray(position, glm::normalize(cast - position));
+                    glm::vec3 radiance(0.0f);
+                    glm::vec3 throughput(1.0f);
 
-                           glm::vec3 radiance(0.0f, 0.0f, 0.0f);
-                           glm::vec3 throughput(1.0f, 1.0f, 1.0f);
+                    for (std::size_t _ = 0; _ < max_bounces; ++_) {
+                        bool success =
+                            scene.interact(ray, radiance, throughput, sampler);
+                        if (!success) {
+                            break;
+                        }
+                    }
 
-                           for (std::size_t _ = 0; _ < max_bounces; ++_) {
-                               bool success = scene.interact(
-                                   ray, radiance, throughput, sampler);
-                               if (!success) {
-                                   break;
-                               }
-                           }
+                    return radiance;
+                });
 
-                           accumulated += radiance;
-                           sampler.step();
-                       }
-
-                       accumulated /= static_cast<float>(pixel_samples);
-                       return accumulated;
-                   });
+            accumulated /= static_cast<float>(pixel_samples);
+            return accumulated;
+        });
 
     output(pixels);
 }
@@ -77,7 +81,7 @@ void hls::Camera::output(const std::vector<glm::vec3> &pixels) const {
         // Reinhard tonemapping
         glm::vec3 tone_mapped = pixel / (pixel + 1.0f);
 
-        // Gamma 2.2 corerction
+        // Gamma 2.2 correction
         glm::vec3 gamma_corrected =
             glm::pow(tone_mapped, glm::vec3(1.0f / 2.2f));
 
